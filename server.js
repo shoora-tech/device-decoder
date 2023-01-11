b6139479bdceb808e52c749ec1b0abc909e7ef40
Avinash Mishra <avinashmishra1904@gmail.com>
2023-01-11 18:14:27 +0530

    added update for latest gps value

--- server.js
+++ server.js
@@ -302,8 +302,7 @@ async function insertSQSDataInDB(data,uuid) {
                                 is_corrupt = EXCLUDED.is_corrupt,
                                 raw_hex_data = EXCLUDED.raw_hex_data,
                                 device_time = EXCLUDED.device_time,
-                                organization_id = EXCLUDED.organization_id,
-                                `;
+                                organization_id = EXCLUDED.organization_id`;
       console.log("update query -> ", update_latest_rt_query)
       client.query(update_latest_rt_query, (err, res) => {
         if (err) {
