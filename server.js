1a4ae62cfd847ba6c1fcd69cba9624f90a8421d3
Avinash Mishra <avinashmishra1904@gmail.com>
2023-02-18 20:05:26 +0530

    added support for isotime

--- server.js
+++ server.js
@@ -287,11 +287,11 @@ async function insertSQSDataInDB(data,uuid) {
       const update_latest_rt_query = `INSERT INTO alert_latestgps (location_packet_type, message_body_length, imei,
                                       message_serial_number, alarm_series, terminal_status,
                                       ignition_status, latitude, longitude, height, speed,
-                                      direction, created_at, updated_at, is_corrupt, raw_hex_data, device_time, organization_id, gps_datetime)
+                                      direction, created_at, updated_at, is_corrupt, raw_hex_data, device_time, organization_id)
                               VALUES (${data.locationPacketType}, '${data.messageBodyLength}',
                               '${data.phoneNumber}', '${data.msgSerialNumber}', '${data.alarmSeries}',
                               '${data.terminalStatus}', ${iStatus}, ${data.latitute}, ${data.longitute},
-                              ${data.height}, ${data.speed}, ${data.direction}, '${date}', '${date}', '${data.is_corrupt}', '${data.raw_hex_data}', '${data.device_time}', ${org_id}, '${data.time}')
+                              ${data.height}, ${data.speed}, ${data.direction}, '${date}', '${date}', '${data.is_corrupt}', '${data.raw_hex_data}', '${data.device_time}', ${org_id})
                               ON CONFLICT (imei)
                               DO
                                 UPDATE SET
